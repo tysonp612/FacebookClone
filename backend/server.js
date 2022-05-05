7faa33e4718485eb140581057b8bbc7192294a65
email <phamson061297@gmail.com>
2022-05-04 22:20:38 -0300

    first commit facebook clone

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("welcome from home");
+});
+app.listen(8000, () => {
+  console.log("listening to port 8000");
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { HomeActive } from "./svg/index";
+function App() {
+  return (
+    <div>
+      <HomeActive />
+      welcome to frontend
+    </div>
+  );
+}
+
+export default App;
