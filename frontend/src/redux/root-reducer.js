5b10f8a5192558616c1f38853e6aebb148fbb2b0
email <phamson061297@gmail.com>
2022-05-18 16:08:34 -0300

    making user reducer and action

--- frontend/src/redux/root-reducer.js
+++ frontend/src/redux/root-reducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
+import { userReducer } from "./reducers/user/user.reducer";
 import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
   //   whitelist: ["cart"],
 };
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  user: userReducer,
+});
 export default persistReducer(persistConfig, rootReducer);
